d5aecde4806a130550786f21f8fdd13c27996e16
Mohammad reza Kamalifard <kamalifard@datasec.ir>
2013-09-14 07:07:29 +0430

    add test.py and copyright comments

---
+++ test.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+from toPersian import  *
+
+print enToPersianNumb('شماره کلاس 312')
+print enToPersianNumb(3123123.9012)
+print enToPersianNumb(123)
+print enToPersianchar('sghl ]i ofv')
+print arToPersianNumb('٣٤٥٦')
+print arToPersianChar(' ك جمهوري اسلامي ايران')
+
+'''
+شماره کلاس ۳۱۲
+۳۱۲۳۱۲۳.۹۰۱۲
+۱۲۳
+سلام چه خبر
+۳۴۵۶
+ ک جمهوری اسلامی ایران
+'''
