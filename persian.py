e022cec82d7df34682d02eba7a7f7b8b45b9b48d
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2013-09-25 23:11:34 +0330

    Update persian.py
    change "replacing" function using "replace" method.

--- persian.py
+++ persian.py
@@ -9,34 +9,33 @@ https://github.com/itmard/persian.py
 def enToPersianNumb(number):
     numPersian = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹','.']
     numEnglish = ['0','1','2','3','4','5','6','7','8','9','.']
+    dictionary = dict(zip(numEnglish),numPersian)
     return replaceing(numEnglish, numPersian, number)
 
 def enToPersianchar(userInput):
     charPersian = [ 'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'چ', 'ش', 'س', 'ی', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ک', 'گ', 'ظ', 'ط', 'ز', 'ر', 'ذ', 'د', 'پ', 'و','؟' ]
     charEnglish = [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',','?' ]
+    dictionary = dict(zip(char(charEnglish),charPersian)
     return replaceing(charEnglish, charPersian, userInput)
 
 def arToPersianNumb(number):
     numArabic = ['١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠']
     numPersian = ['۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۰']
+    dictionary = dict(zip(nupArabic),NumPersian)
     return replaceing(numArabic, numPersian, number)
 
 def arToPersianChar(userInput):
     charArabic = ['ي', 'ك', '‍', 'دِ', 'بِ', 'زِ', 'ذِ', 'ِشِ', 'ِسِ', '‌', 'ى']
     charPersian = ['ی', 'ک', '', 'د', 'ب', 'ز', 'ذ', 'ش', 'س', '', 'ی']
+    dictionary = dict(zip(charArabic),charPersian)
     return replaceing(charArabic, charPersian, userInput)
     
 #list 1 is or  not ok host
 #list 2 is or ok dest
 
-def replaceing(host, dest, input):
+def replaceing(input,dictionary):
     
-    returnList = list()
-
-    for i in list(unicode(input)):
-        if i in host:
-            returnList.append(dest[host.index(i)])
-        else:
-            returnList.append(i)
-    
-    return ''.join(returnList)
+    for i in dictionary:
+        transed = input.replace(i,dictionary[i])
+        input = transed
+    return input
