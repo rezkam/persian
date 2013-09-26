3c8f7580afc60344002f688c0bab2ea6f14d3c6e
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2013-09-26 13:34:20 +0330

    Update persian.py
    correction function requests.

--- persian.py
+++ persian.py
@@ -10,25 +10,25 @@ def enToPersianNumb(number):
     numPersian = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹','.']
     numEnglish = ['0','1','2','3','4','5','6','7','8','9','.']
     dictionary = dict(zip(numEnglish),numPersian)
-    return replaceing(numEnglish, numPersian, number)
+    return replaceing(number,dictionary)
 
 def enToPersianchar(userInput):
     charPersian = [ 'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'چ', 'ش', 'س', 'ی', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ک', 'گ', 'ظ', 'ط', 'ز', 'ر', 'ذ', 'د', 'پ', 'و','؟' ]
     charEnglish = [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',','?' ]
     dictionary = dict(zip(charEnglish),charPersian)
-    return replaceing(charEnglish, charPersian, userInput)
+    return replaceing(userInput,dictionary)
 
 def arToPersianNumb(number):
     numArabic = ['١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩', '٠']
     numPersian = ['۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۰']
     dictionary = dict(zip(nupArabic),NumPersian)
-    return replaceing(numArabic, numPersian, number)
+    return replaceing(number,dictionary)
 
 def arToPersianChar(userInput):
     charArabic = ['ي', 'ك', '‍', 'دِ', 'بِ', 'زِ', 'ذِ', 'ِشِ', 'ِسِ', '‌', 'ى']
     charPersian = ['ی', 'ک', '', 'د', 'ب', 'ز', 'ذ', 'ش', 'س', '', 'ی']
     dictionary = dict(zip(charArabic),charPersian)
-    return replaceing(charArabic, charPersian, userInput)
+    return replaceing(userInput,dictionary)
     
 #list 1 is or  not ok host
 #list 2 is or ok dest
