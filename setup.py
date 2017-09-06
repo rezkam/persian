cd86eef23930b598f0b6670b7332bfaa1544a8eb
Mohammad Reza Kamalifard <mrkamalifard@gmail.com>
2017-09-06 17:10:03 +0430

    Update setup file  name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="Persian",
+    name="persian",
     version="0.4.0",
     author="Mohammad Reza Kamalifard",
     author_email="mrkamalifard@gmail.com",
     description=("Simple Python library for Persian language localization"),
     license="Apache License 2.0",
     keywords="example documentation tutorial",
-    url="http://packages.python.org/persian",
+    url="https://github.com/itmard/Persian",
     packages=['persian'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
