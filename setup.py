75218a819993cdbc9559aea7ba0b76f0e46640ae
Mohammad Reza Kamalifard <mrkamalifard@gmail.com>
2017-09-06 16:24:47 +0430

    update setup file and package version to 0.4.0

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-import os
 from setuptools import setup
 
 setup(
-    name = "persian",
-    version = "0.0.3",
-    author = "Mohammad Reza Kamalifard",
-    author_email = "mr.kamalifard@gmail.com",
-    description = ("A simple Python library for Persian language localization"),
-    license = "BSD",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/persian",
+    name="Persian",
+    version="0.4.0",
+    author="Mohammad Reza Kamalifard",
+    author_email="mrkamalifard@gmail.com",
+    description=("Simple Python library for Persian language localization"),
+    license="Apache License 2.0",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/persian",
     packages=['persian'],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python"
     ],
 )
