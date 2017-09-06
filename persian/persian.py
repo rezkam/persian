05aeb1565c2fbab775018f970df06768e4b3d1b2
Mohammad Reza Kamalifard <mrkamalifard@gmail.com>
2017-09-06 16:11:11 +0430

    Add new function names and depricate old ones

---
+++ persian/persian.py
@@ -0,0 +1,130 @@
+# encoding: utf-8
+
+"""
+Persian
+Simple tool for Persian language localization in Python
+Copyright (C) 2017 Mohammad Reza Kamalifard (mrkamalifard@gmail.com)
+@rezakamalifard
+MIT licensed
+https://github.com/itmard/Persian
+"""
+
+import re
+
+
+def convert_en_numbers(input_str):
+    """
+    Converts English numbers to Persian numbers
+    :param input_str: String contains English numbers
+    :return: New string with Persian numbers
+    """
+    mapping = {
+        '0': '۰',
+        '1': '۱',
+        '2': '۲',
+        '3': '۳',
+        '4': '۴',
+        '5': '۵',
+        '6': '۶',
+        '7': '۷',
+        '8': '۸',
+        '9': '۹',
+        '.': '.',
+    }
+    return _multiple_replace(mapping, input_str)
+
+
+def convert_en_characters(input_str):
+    """
+        Assumes that characters written with standard persian keyboard
+        not windows arabic layout
+    :param input_str: String contains English chars
+    :return: New string with related characters on Persian standard keyboard
+    """
+    mapping = {
+        'q': 'ض',
+        'w': 'ص',
+        'e': 'ث',
+        'r': 'ق',
+        't': 'ف',
+        'y': 'غ',
+        'u': 'ع',
+        'i': 'ه',
+        'o': 'خ',
+        'p': 'ح',
+        '[': 'ج',
+        ']': 'چ',
+        'a': 'ش',
+        's': 'س',
+        'd': 'ی',
+        'f': 'ب',
+        'g': 'ل',
+        'h': 'ا',
+        'j': 'ت',
+        'k': 'ن',
+        'l': 'م',
+        ';': 'ک',
+        "'": 'گ',
+        'z': 'ظ',
+        'x': 'ط',
+        'c': 'ز',
+        'v': 'ر',
+        'b': 'ذ',
+        'n': 'د',
+        'm': 'پ',
+        ',': 'و',
+        '?': '؟',
+    }
+    return _multiple_replace(mapping, input_str)
+
+
+def convert_ar_numbers(input_str):
+    """
+    Converts Arabic numbers to Persian numbers
+    :param input_str: String contains Arabic numbers
+    :return: New str and replaces arabic number with persian numbers
+    """
+    mapping = {
+        '١': '۱',  # Arabic 1 is 0x661 and Persian one is 0x6f1
+        '٢': '۲',  # More info https://goo.gl/SPiBtn
+        '٣': '۳',
+        '٤': '۴',
+        '٥': '۵',
+        '٦': '۶',
+        '٧': '۷',
+        '٨': '۸',
+        '٩': '۹',
+        '٠': '۰',
+    }
+    return _multiple_replace(mapping, input_str)
+
+
+def convert_ar_characters(input_str):
+    """
+    Converts Arabic chars to related Persian unicode char
+    :param input_str: String contains Arabic chars
+    :return: New str with converted arabic chars
+    """
+    mapping = {
+        'ك': 'ک',
+        'دِ': 'د',
+        'بِ': 'ب',
+        'زِ': 'ز',
+        'ذِ': 'ذ',
+        'شِ': 'ش',
+        'سِ': 'س',
+        'ى': 'ی',
+        'ي': 'ی'
+    }
+    return _multiple_replace(mapping, input_str)
+
+
+def _multiple_replace(mapping, text):
+    """
+    Internal function for replace all mapping keys for a input string
+    :param mapping: replacing mapping keys
+    :param text: user input string
+    :return: New string with converted mapping keys to values
+    """
+    pattern = "|".join(map(re.escape, mapping.keys()))
+    return re.sub(pattern, lambda m: mapping[m.group()], str(text))
