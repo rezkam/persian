ae9188c5aec4aad804774b30b0f55f536f357bf3
Mohammad Hasanzadeh <23486263+mohammadhasanzadeh@users.noreply.github.com>
2025-11-06 12:19:14 +0330

    overal performance improvements

--- persian/persian.py
+++ persian/persian.py
@@ -19,19 +19,20 @@ def convert_en_numbers(input_str):
     :param input_str: String contains English numbers
     :return: New string with Persian numbers
     """
-    mapping = {
-        '0': '۰',
-        '1': '۱',
-        '2': '۲',
-        '3': '۳',
-        '4': '۴',
-        '5': '۵',
-        '6': '۶',
-        '7': '۷',
-        '8': '۸',
-        '9': '۹',
-        '.': '.',
-    }
+    mapping = (
+        ('0', '۰'),
+        ('1', '۱'),
+        ('2', '۲'),
+        ('3', '۳'),
+        ('4', '۴'),
+        ('5', '۵'),
+        ('6', '۶'),
+        ('7', '۷'),
+        ('8', '۸'),
+        ('9', '۹'),
+        ('.', '.'),
+        )
+
     return _multiple_replace(mapping, input_str)
 
 
@@ -42,40 +43,41 @@ def convert_en_characters(input_str):
     :param input_str: String contains English chars
     :return: New string with related characters on Persian standard keyboard
     """
-    mapping = {
-        'q': 'ض',
-        'w': 'ص',
-        'e': 'ث',
-        'r': 'ق',
-        't': 'ف',
-        'y': 'غ',
-        'u': 'ع',
-        'i': 'ه',
-        'o': 'خ',
-        'p': 'ح',
-        '[': 'ج',
-        ']': 'چ',
-        'a': 'ش',
-        's': 'س',
-        'd': 'ی',
-        'f': 'ب',
-        'g': 'ل',
-        'h': 'ا',
-        'j': 'ت',
-        'k': 'ن',
-        'l': 'م',
-        ';': 'ک',
-        "'": 'گ',
-        'z': 'ظ',
-        'x': 'ط',
-        'c': 'ز',
-        'v': 'ر',
-        'b': 'ذ',
-        'n': 'د',
-        'm': 'پ',
-        ',': 'و',
-        '?': '؟',
-    }
+    mapping = (
+        ('q', 'ض'),
+        ('w', 'ص'),
+        ('e', 'ث'),
+        ('r', 'ق'),
+        ('t', 'ف'),
+        ('y', 'غ'),
+        ('u', 'ع'),
+        ('i', 'ه'),
+        ('o', 'خ'),
+        ('p', 'ح'),
+        ('[', 'ج'),
+        (']', 'چ'),
+        ('a', 'ش'),
+        ('s', 'س'),
+        ('d', 'ی'),
+        ('f', 'ب'),
+        ('g', 'ل'),
+        ('h', 'ا'),
+        ('j', 'ت'),
+        ('k', 'ن'),
+        ('l', 'م'),
+        (';', 'ک'),
+        ("'", 'گ'),
+        ('z', 'ظ'),
+        ('x', 'ط'),
+        ('c', 'ز'),
+        ('v', 'ر'),
+        ('b', 'ذ'),
+        ('n', 'د'),
+        ('m', 'پ'),
+        (',', 'و'),
+        ('?', '؟'),
+        )
+
     return _multiple_replace(mapping, input_str)
 
 
@@ -85,18 +87,19 @@ def convert_ar_numbers(input_str):
     :param input_str: String contains Arabic numbers
     :return: New str and replaces arabic number with persian numbers
     """
-    mapping = {
-        '١': '۱',  # Arabic 1 is 0x661 and Persian one is 0x6f1
-        '٢': '۲',  # More info https://goo.gl/SPiBtn
-        '٣': '۳',
-        '٤': '۴',
-        '٥': '۵',
-        '٦': '۶',
-        '٧': '۷',
-        '٨': '۸',
-        '٩': '۹',
-        '٠': '۰',
-    }
+    mapping = (
+        ('١', '۱'), # Arabic 1 is 0x661 and Persian one is 0x6f1
+        ('٢', '۲'), # More info https://goo.gl/SPiBtn
+        ('٣', '۳'),
+        ('٤', '۴'),
+        ('٥', '۵'),
+        ('٦', '۶'),
+        ('٧', '۷'),
+        ('٨', '۸'),
+        ('٩', '۹'),
+        ('٠', '۰'),
+        )
+
     return _multiple_replace(mapping, input_str)
 
 
@@ -109,19 +112,20 @@ def convert_fa_numbers(input_str):
 
     Returns: English numbers
     """
-    mapping = {
-        '۰': '0',
-        '۱': '1',
-        '۲': '2',
-        '۳': '3',
-        '۴': '4',
-        '۵': '5',
-        '۶': '6',
-        '۷': '7',
-        '۸': '8',
-        '۹': '9',
-        '.': '.',
-    }
+    mapping = (
+        ('۰', '0'),
+        ('۱', '1'),
+        ('۲', '2'),
+        ('۳', '3'),
+        ('۴', '4'),
+        ('۵', '5'),
+        ('۶', '6'),
+        ('۷', '7'),
+        ('۸', '8'),
+        ('۹', '9'),
+        ('.', '.'),
+        )
+
     return _multiple_replace(mapping, input_str)
 
 
@@ -131,17 +135,18 @@ def convert_ar_characters(input_str):
     :param input_str: String contains Arabic chars
     :return: New str with converted arabic chars
     """
-    mapping = {
-        'ك': 'ک',
-        'دِ': 'د',
-        'بِ': 'ب',
-        'زِ': 'ز',
-        'ذِ': 'ذ',
-        'شِ': 'ش',
-        'سِ': 'س',
-        'ى': 'ی',
-        'ي': 'ی'
-    }
+    mapping = (
+        ('ك', 'ک'),
+        ('دِ', 'د'),
+        ('بِ', 'ب'),
+        ('زِ', 'ز'),
+        ('ذِ', 'ذ'),
+        ('شِ', 'ش'),
+        ('سِ', 'س'),
+        ('ى', 'ی'),
+        ('ي', 'ی')
+        )
+
     return _multiple_replace(mapping, input_str)
 
 
@@ -176,9 +181,11 @@ def decode_url(input_str):
 def _multiple_replace(mapping, text):
     """
     Internal function for replace all mapping keys for a input string
-    :param mapping: replacing mapping keys
+    :param mapping: replacing mapping tuple
     :param text: user input string
     :return: New string with converted mapping keys to values
     """
-    pattern = "|".join(map(re.escape, mapping.keys()))
-    return re.sub(pattern, lambda m: mapping[m.group()], str(text))
+
+    for map in mapping:
+        text = text.replace(*map)
+    return text
